e38ae9ca98081f5dd11fb4a2c02785bb737540a1
danielcordeirofaria <danielcordeirofaria@yahoo.com.br>
2024-07-29 15:49:09 -0300

    atualizando versão angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,35 +6,27 @@ import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AlunosCadastroComponent } from './components/alunos-cadastro/alunos-cadastro.component';
 import { CpfFormatDirective } from './cpf-format.directive';
 import { ConsultarAlunosComponent } from './components/consultar-alunos/consultar-alunos.component';
 import { AlunoDetalhesComponent } from './components/aluno-detalhes/aluno-detalhes.component';
 import { CommonModule } from '@angular/common';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    AppComponent,
-    HeaderComponent,
-    MainComponent,
-    FooterComponent,
-    AlunosCadastroComponent,
-    CpfFormatDirective,
-    ConsultarAlunosComponent,
-    AlunoDetalhesComponent
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    CommonModule 
-
-  ]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        AppComponent,
+        HeaderComponent,
+        MainComponent,
+        FooterComponent,
+        AlunosCadastroComponent,
+        CpfFormatDirective,
+        ConsultarAlunosComponent,
+        AlunoDetalhesComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        CommonModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
